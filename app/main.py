6b30ef828408c1808a1ca844f164641c2b308e06
caydenredding <caydenredding0@gmail.com>
2026-02-11 11:35:17 +0000

    added the includes for the new tables

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app import models
-from app.routers import users, exercises, workouts, exercise_logs
+from app.routers import users, exercises, workouts, exercise_logs, muscles, equipment, sets
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,8 +9,12 @@ app = FastAPI(title="Workout Tracker API")
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(exercises.router, prefix="/exercises", tags=["Exercises"])
-app.include_router(workouts.router, prefix="/workouts", tags=["Workouts"])
+app.include_router(workouts.router, tags=["Workouts"])
 app.include_router(exercise_logs.router, tags=["Exercise Logs"])
+app.include_router(muscles.router, prefix="/muscles", tags=["Muscles"])
+app.include_router(equipment.router, prefix="/equipment", tags=["Equipment"])
+app.include_router(sets.router, tags=["Sets"])
+
 
 @app.get("/")
 def root():
