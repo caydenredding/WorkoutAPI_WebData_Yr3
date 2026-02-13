73ddc0b6b6066d4273edc48db38509651943f556
caydenredding <caydenredding0@gmail.com>
2026-02-13 14:18:27 +0000

    added ai API router

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 from app.routers.training import router as training_router
 from app.routers.catalog import router as catalog_router
 from app.routers.users import router as users_router
-from app.routers.analytics import router as analytics_router 
+from app.routers.analytics import router as analytics_router
+from app.routers.ai import router as ai_router
 
 app = FastAPI(
     title="Gym API",
@@ -14,6 +15,7 @@ app.include_router(training_router)
 app.include_router(catalog_router)
 app.include_router(users_router)
 app.include_router(analytics_router)
+app.include_router(ai_router)
 
 
 @app.get(
