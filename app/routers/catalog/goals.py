cd1054a1cfd33d643cd1fabb10934a3b677554e4
caydenredding <caydenredding0@gmail.com>
2026-02-16 14:55:37 +0000

    added a feature for users to store their current goals

---
+++ app/routers/catalog/goals.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.database import get_db
+from app import models, schemas
+
+router = APIRouter(prefix="/goals")
+
+
+@router.get(
+    "/",
+    response_model=List[schemas.GoalOut],
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {"description": "List of goals returned"},
+    },
+)
+def list_goals(db: Session = Depends(get_db)):
+    return db.query(models.Goal).order_by(models.Goal.name.asc()).all()
